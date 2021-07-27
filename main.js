7e484067210ae5b2f1161f4077864ced2580c14d
JStan811 <jakestandley95@gmail.com>
2021-07-27 15:37:39 -0500

    Button now does change grid based on user input, but has some issues. I think I need to first remove all the existing divs before adding new ones.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const container = document.querySelector('.container');
 
 //change color function to be used by event listener
 function changeColor(e) {
-    this.style.backgroundColor = 'black';
+    this.style.backgroundColor = '#757575';
 }
 
 //create div with class gridCell, add event listener on mouseover that runs changeColor(), then insert it into container
@@ -19,11 +19,18 @@ function addCell() {
     container.appendChild(gridCell);
 }
 
-//create 256 (16x16) divs and place inside "container" div
-for(i = 1; i <= 256; i++) {
-    addCell();
+function setGrid(squaresPerSide) {
+    let totalSquares = squaresPerSide * squaresPerSide;
+    for(i = 1; i <= totalSquares; i++) {
+        addCell();
+    }
+    container.style['grid-template-columns'] = `repeat(${squaresPerSide}, 1fr)`;
+    container.style['grid-template-rows'] = `repeat(${squaresPerSide}, 1fr)`;
 }
 
+//create 256 (16x16) divs and place inside "container" div
+
+setGrid(16);
 
 //code for button allowing user to set own grid size
 const btn = document.querySelector('button');
@@ -33,21 +40,20 @@ function changeGrid(e) {
     
     //loop to make sure user's input is an integer and max 100
     while(true) {
-        userInput = parseFloat(prompt('How many square per side would you like for your grid?', '16')); //conversion to float needed to check isInteger later
-        if (userInput >= 100) {
+        userInput = parseFloat(prompt('How many squares per side would you like for your grid?', '16')); //conversion to float needed to check isInteger later
+        if (userInput > 100) {
             alert("Sorry! Your grid can only have up to 100 squares per side.");
             continue;
         }
-        if (!(Number.isInteger(userInput))) { //here is where we check to make sure the input is an integer
-            alert("Please enter a whole number.");
+        if (!(Number.isInteger(userInput)) || userInput < 1) { 
+            alert("Please enter an integer greater than 0.");
             continue;
         }
         console.log(userInput);
         break;
     }
 
-    
-
+    setGrid(userInput);
 }
 
 btn.addEventListener("click", changeGrid);
